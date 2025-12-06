b34d43a930a5bd0df0781e8110faaa1331f07e73
Sangeetha Manoharan <sangeemano25@gmail.com>
2025-12-07 00:27:53 +0100

    Assignment 1 - Data Types

---
+++ src/language/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package language.basics;
+
+public class HelloWorld {
+
+	
+	  public static void main(String[] args) { // TODO Auto-generated method stub
+	  System.out.println("Hello World"); //Sysout+Ctrl+space and Ctrl+click ->to see Internal code
+	  
+	  }
+	 
+
+}
